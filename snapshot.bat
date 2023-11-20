23dcff3dcf22a9331209f9c7b21237330622011a
larsaur <lars.m.aurud@gmail.com>
2023-11-20 22:02:25 +0100

    Add support for version number in uci id name. If none is given, use dev_build.

--- snapshot.bat
+++ snapshot.bat
@@ -1,10 +1,14 @@
 @ECHO OFF
 ECHO Creating snapshot
 
-set CFLAGS=-DPRINT_TO_FILE
-set /p PROJECT=Name of the snapshot: 
+if [%1] == [] echo "usage error 'snapshot.bat [filename] <version name>' "
+
+set PROJECT=%1
+
+if [%2] == [] (set FLAGS=-DPRINT_TO_FILE) else (set FLAGS="-DPRINT_TO_FILE -DARCANUM_VERSION=%2")
 
 make clean
+make -j CFLAGS=%FLAGS%
 make -j
 copy /b ".\build\%PROJECT%.exe" ".\snapshots\%PROJECT%.exe" /b
 make clean
