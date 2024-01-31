8e99bd6d0b336a100d99d77aaf0be72e7b52ea25
larsaur <lars.m.aurud@gmail.com>
2024-01-31 23:48:50 +0100

    Fix passing version argument to snapshot

--- snapshot.bat
+++ snapshot.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+setlocal enabledelayedexpansion
 
 if [%1] == [] echo "usage error 'snapshot.bat [filename] <version name>' "
 if [%1] == [] EXIT /B -1
@@ -6,12 +7,18 @@ if [%1] == [] EXIT /B -1
 ECHO Creating snapshot
 set PROJECT=%1
 
-set "FLAGS=-DDISABLE_DEBUG -DDISABLE_LOG -DDISABLE_WARNING -DDISABLE_ERROR -DDISABLE_SUCCESS"
-if [%2] NEQ [] (set FLAGS="%FLAGS% -DARCANUM_VERSION=%2")
+set FLAGS=-DDISABLE_DEBUG -DDISABLE_LOG -DPRINT_TO_FILE
+if NOT "%2"=="" (
+    ECHO Setting version %2
+    set VERSION=-DARCANUM_VERSION=%2
+    set FLAGS="%FLAGS% !VERSION!"
+    set FLAGS=!FLAGS:~1,-1!
+) else (
+    ECHO No version set, using dev_version
+)
 
 make clean
-make -j CFLAGS="%FLAGS%"
-make -j
+make -j CFLAGS="!FLAGS!"
 copy ".\build\%PROJECT%.exe" /b ".\snapshots\%PROJECT%.exe" /b
 copy "nn-04cf2b4ed1da.nnue" /b ".\snapshots\nn-04cf2b4ed1da.nnue" /b
 copy "hceWeights.dat" /b ".\snapshots\hceWeights.dat" /b
