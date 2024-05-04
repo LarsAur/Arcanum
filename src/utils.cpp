32cb316b4d785845d071ecca01e98f9ce456303d
LarsAur <lars1998@hotmail.com>
2024-05-04 11:59:41 +0200

    Add OS specific implementation of getWorkPath()

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,22 +1,30 @@
 #include <utils.hpp>
 
+#if defined(_WIN64)
+#include <Libloaderapi.h>
+#elif defined(__linux__)
+#include <filesystem>
+#endif
+
 // Gets the path to the folder which the executable is in
 std::string getWorkPath()
 {
     // Get the path of the executable file
-    char execFullPath[2048];
     #if defined(_WIN64)
+        char execFullPath[2048];
         GetModuleFileNameA(NULL, execFullPath, 2048);
+        // Move one folder up
+        std::string path = std::string(execFullPath);
+        size_t idx = path.find_last_of('\\');
+        path = std::string(path).substr(0, idx + 1); // Keep the last '\'
+        return path;
     #elif defined(__linux__)
-        ERROR("Missing implementation")
+        std::filesystem::path absPath = std::filesystem::canonical("/proc/self/exe");
+        std::string path = std::string(absPath.parent_path().c_str());
+        path.append("/");
+        return path;
     #else
-        ERROR("Missing implementation")
+        #error Missing implementation of getWorkPath
     #endif
 
-    // Move one folder up
-    std::string path = std::string(execFullPath);
-    size_t idx = path.find_last_of('\\');
-    path = std::string(path).substr(0, idx + 1); // Keep the last '\'
-
-    return path;
 }
