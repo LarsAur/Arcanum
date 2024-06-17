d022846d2e2a820a2d5741605d75abfed38f613e
larsaur <lars.m.aurud@gmail.com>
2024-06-17 20:38:00 +0200

    Reduce search depth to one when there is only one legal move in the root

--- src/search.cpp
+++ src/search.cpp
@@ -534,6 +534,14 @@ Move Searcher::search(Board board, SearchParameters parameters, SearchResult* se
         }
     }
 
+    // If only one move is available, search it at depth 1 to not waist time
+    // but still give it a score and pv-line
+    if(numMoves == 1)
+    {
+        m_searchParameters.depth = 1;
+        m_searchParameters.useDepth = true;
+    }
+
     uint32_t depth = 0;
     while(!m_searchParameters.useDepth || m_searchParameters.depth > depth)
     {
