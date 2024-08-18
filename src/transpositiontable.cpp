0c7e2c363f93c0a08e7693934d7232fdf556801b
larsaur <lars.m.aurud@gmail.com>
2024-08-18 18:43:55 +0200

    Fix segfault: Avoid prefetching when the TT size is 0

--- src/transpositiontable.cpp
+++ src/transpositiontable.cpp
@@ -91,7 +91,8 @@ inline size_t TranspositionTable::m_getClusterIndex(hash_t hash)
 
 void TranspositionTable::prefetch(hash_t hash)
 {
-    _mm_prefetch(m_table + m_getClusterIndex(hash), _MM_HINT_T0);
+    if(m_table)
+        _mm_prefetch(m_table + m_getClusterIndex(hash), _MM_HINT_T0);
 }
 
 std::optional<ttEntry_t> TranspositionTable::get(hash_t hash, uint8_t plyFromRoot)
