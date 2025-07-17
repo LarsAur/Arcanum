6943e0be6069da11c94c78259a9231be583721ee
larsaur <lars.m.aurud@gmail.com>
2025-07-17 13:40:33 +0200

    Fix warnings: use \x1B as ASCII escape character instead if \e

--- src/utils.hpp
+++ src/utils.hpp
@@ -9,14 +9,14 @@
 #define STRINGIFY(s) #s
 #define TOSTRING(x) STRINGIFY(x)
 
-#define COLOR_BLACK "\e[0;30m"
-#define COLOR_RED "\e[0;31m"
-#define COLOR_GREEN "\e[0;32m"
-#define COLOR_YELLOW "\e[0;33m"
-#define COLOR_BLUE "\e[0;34m"
-#define COLOR_PURPLE "\e[0;35m"
-#define COLOR_CYAN "\e[0;36m"
-#define COLOR_WHITE "\e[0;37m"
+#define COLOR_BLACK "\x1B[0;30m"
+#define COLOR_RED "\x1B[0;31m"
+#define COLOR_GREEN "\x1B[0;32m"
+#define COLOR_YELLOW "\x1B[0;33m"
+#define COLOR_BLUE "\x1B[0;34m"
+#define COLOR_PURPLE "\x1B[0;35m"
+#define COLOR_CYAN "\x1B[0;36m"
+#define COLOR_WHITE "\x1B[0;37m"
 
 #define DEBUG_COLOR COLOR_CYAN
 #define LOG_COLOR COLOR_WHITE
